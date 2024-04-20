f3dee220d9d09131895dbc922b3a65c978d4c251
shohsulton <nniktoev141@gmail.com>
2024-04-20 15:25:34 +0500

    login.py created

---
+++ login.py
@@ -0,0 +1,34 @@
+users = [
+    {
+        'username': 'admin',
+        'password': '123',
+    },
+    {
+        'username': "john",
+        'password': 'admin123'
+    }
+]
+
+
+def get_user_by_username(username):
+    for user in users:
+        if user['username'] == username:
+            return user
+    return None
+
+
+def login():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    user = get_user_by_username(username)
+    if not user:
+        print("User not found")
+    else:
+        if user['password'] == password:
+            print("Login successfully")
+        else:
+            print('Login failed')
+
+
+if __name__ == '__main__':
+    login()
